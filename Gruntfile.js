ebe29cd0d9453cc3e8cc585a1d27a2be1479a79f
Jakub Turek <ja@kubaturek.pl>
2016-01-16 13:14:58 +0100

    Fix path with unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
                 options: {
                     reporter: 'progress'
                 },
-                src: ['dist/test/**/*.js']
+                src: ['build/test/**/*.js']
             }
         },
         tslint: {
